05789b48fa9c8fb369afd3bfc2186934da98b667
Qianyi Wang <wangqianyi.cs@gmail.com>
2016-06-29 00:36:37 +0800

    implement staic page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+  get 'static_pages/about'
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  #root 'users#index'
 end
