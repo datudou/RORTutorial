66d8a473f0e82937bf42b475891e2b6f0e1693cf
Qianyi Wang <wangqianyi.cs@gmail.com>
2016-07-11 13:40:12 +0800

    create a basic user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627092322) do
+ActiveRecord::Schema.define(version: 20160710095300) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -22,8 +22,10 @@ ActiveRecord::Schema.define(version: 20160627092322) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
