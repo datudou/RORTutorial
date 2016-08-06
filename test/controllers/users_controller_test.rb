d50b2fe09b066be5fdb02bf911a205710d02756a
Qianyi Wang <wangqianyi.cs@gmail.com>
2016-08-07 00:24:49 +0800

    Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
     @user = users(:michael)
     @other_user = users(:archer)
   end
@@ -36,4 +35,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   #   assert_not flash.empty?
   #   assert_redirected_to login_url
   # end
+
+  test "should redirect destroy when not logged in" do
+   assert_no_difference 'User.count' do
+     delete user_path(@user)
+   end
+   assert_redirected_to login_url
+ end
+
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 end
